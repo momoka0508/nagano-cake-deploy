d7b379d2be71c075c8f0ab18f3973f1806cc9ed1
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-17 16:41:58 +0000

    [Add] customer バリデーション

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,16 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :addresses, dependent: :destroy
+  
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :email, presence: true
+  validates :zip_code, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  
 end
