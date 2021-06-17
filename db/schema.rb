94ad1fbf73bc4a5e5be3c3d6c74db4f34a3ead2d
momoka0508 <y.mmk.0508@gmail.com>
2021-06-17 05:12:24 +0000

    [Add]cart_items_setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210617034946) do
+ActiveRecord::Schema.define(version: 20210617050240) do
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer  "customer_id"
+    t.integer  "item_id"
+    t.integer  "quantity"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
