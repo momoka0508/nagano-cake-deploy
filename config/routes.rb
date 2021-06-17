a6bf04f415ac40df5bb8ba3666771848c9b7c953
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-17 05:30:13 +0000

    [Add] 管理者・会員ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  scope module: 'public' do
+    resource :customers do
+    end
+  end
+  
+  namespace :admin do
+    resources :customers
+  end
+  
 end
