84b93c38770f158c22881bf5a3601e40da26353e
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-17 18:09:19 +0000

    [Add] addressesルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'homes#top'
   get '/home/about' => 'homes#about'
-  
+
   scope module: 'public' do
     resource :customers do
       get 'my_page' => 'customers#show'
     end
+    resources :addresses
     resources :orders, only:[:new, :create, :index, :show]
     resources :cart_items, only:[:index, :update, :create, :destroy]
     resources :items, only:[:index, :show]
@@ -16,7 +17,7 @@ Rails.application.routes.draw do
     get "orders/complete" => "orders#complete"
     delete "cart_items" => "cart_items#destroy_all"
   end
-  
+
   namespace :admin do
     resources :customers
     resources :items, except: [:edit]
