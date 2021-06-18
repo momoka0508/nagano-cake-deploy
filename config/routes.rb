1f59fcfc9077321ba94fcba982b0228e630aae0e
M-Abe47 <abuzhengze5@gmail.com>
2021-06-18 02:35:18 +0000

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
-Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'homes#top'
   get '/home/about' => 'homes#about'
-  
+
   scope module: 'public' do
-    resource :customers
+    devise_for :customers
+    resource :customers do
+      get 'my_page' => 'customers#show'
+      get 'confirm'
+    end
+    resources :addresses
     resources :orders, only:[:new, :create, :index, :show]
     resources :cart_items, only:[:index, :update, :create, :destroy]
     resources :items, only:[:index, :show]
@@ -14,6 +18,10 @@ Rails.application.routes.draw do
     get "orders/complete" => "orders#complete"
     delete "cart_items" => "cart_items#destroy_all"
   end
+  
+  devise_for :admins, only: [:sessions], :controllers => {
+    :sessions => 'admin/sessions'
+  }
   
   namespace :admin do
     resources :customers
