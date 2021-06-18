f95c60a2c0edafc767f33e7d947f307a3f5bf983
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-18 13:33:24 +0000

    [Fix]

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,27 +1,27 @@
 class Public::CustomersController < ApplicationController
-  
+
   def show
     @customer = current_customer
   end
-  
+
   def edit
     @customer = current_customer
   end
-  
+
   def update
     @customer = current_customer
-    if @customer.update(customer_params)
+    if @customer.update(customers_params)
       redirect_to my_page_customers_path
     else
       render :edit
     end
   end
-  
+
   private
-  
+
   def customers_params
     params.require(:customer).permit(
       :first_name, :last_name, :first_name_kana, :last_name_kana, :zip_code, :address, :phone_number, :email)
   end
-  
+
 end
