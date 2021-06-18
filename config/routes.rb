09f78ccf8467e00a83d93e91e9f688c5bb182594
M-Abe47 <abuzhengze5@gmail.com>
2021-06-18 03:07:01 +0000

    ルートを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
-
-  Rails.application.routes.draw do
+Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'homes#top'
@@ -23,7 +22,7 @@
   devise_for :admins, only: [:sessions], :controllers => {
     :sessions => 'admin/sessions'
   }
-
+  
   namespace :admin do
     resources :customers
     resources :items, except: [:edit]
