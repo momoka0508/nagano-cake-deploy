5f0db095b67259e6175ce53e958ae3d7b4ea0bac
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-18 13:47:07 +0000

    [Add] index・createアクション addressesコントローラー

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,19 @@
 class Public::AddressesController < ApplicationController
+  
+  def index
+    @address = Address.new
+    @addresses = Address.where(customer: current_customer)
+  end
+  
+  def create
+    @address = Address.new(address_params)
+    @address.customer = current_customer
+    if @address.save
+      redirect_to addresses_path
+    else
+      @addresses = Address.where(customer: current_customer)
+      render :index
+    end
+  end
+  
 end
