397e4ee178f396d8e911924ce105f82310ba900a
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-19 00:09:23 +0000

    [Test] 外部キー設定

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -2,12 +2,12 @@ class Public::AddressesController < ApplicationController
   
   def index
     @address = Address.new
-    @addresses = Address.where(customer: current_customer)
+    @addresses = Address.where(customer_id: current_customer.id)
   end
   
   def create
     @address = Address.new(address_params)
-    @address.customer = current_customer
+    @address.customer_id = current_customer.id
     if @address.save
       redirect_to addresses_path
     else
