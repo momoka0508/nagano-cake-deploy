3f7851c66a191dfc8bdd15080ce8d11f478c30f8
momoka0508 <y.mmk.0508@gmail.com>
2021-06-19 01:50:31 +0000

    [Add]cart_function

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,9 +3,11 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :addresses, dependent: :destroy
-  
+  has_many :orders, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
@@ -14,5 +16,5 @@ class Customer < ApplicationRecord
   validates :zip_code, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
-  
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+	belongs_to :customer
+	has_many :order_items, dependent: :destroy
+
+	enum pay_method: { yes: 0, no: 1 }
 end
