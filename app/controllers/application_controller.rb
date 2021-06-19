32c2801e5b6600729a72043110ec11f38758cff1
momoka0508 <y.mmk.0508@gmail.com>
2021-06-19 07:15:59 +0000

    [Add]cart_items_create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
   helper_method :current_cart
-  
+
   def current_cart
   	if session[:cart_id]
-  		@cart = Cart.find(session[:cart_id])
+  		@cart_item = CartItem.find(session[:cart_id])
   	else
-  		@cart = Cart.create
-  		session[:cart_id] = @cart.id
+  		@cart_item = CartItem.create
+  		session[:cart_id] = @cart_item.id
   	end
   end
 end
--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,20 +3,20 @@ class Public::CartItemsController < ApplicationController
 	before_action :setup_cart_item!, only: [:update, :create, :destroy, :destroy_all]
 
 	def index
-		@item=Item.find_by(params[:id],params[:item_id])
-		@cart_items=current_cart.cart_items
+		@cart_items=current_cart
+	end
+
+	def create
+		@cart_item=CartItem.new(cart_item_params)
+		@cart_item.customer_id = current_user.id
+		@cart_item.save
+		redirect_to cart_items_path
 	end
 
 	def update
 		cart_item=CartItem.find(params[:quantity])
 		cart_item.update(cart_item_params)
 		redirect_back(fallback_location: root_path)
-	end
-
-	def create
-		cart_item=CartItem.new(cart_item_params)
-		cart_item.save
-		redirect_to cart_items_path
 	end
 
 	def destroy
@@ -27,6 +27,10 @@ class Public::CartItemsController < ApplicationController
 	end
 
 private
+
+def setup_cart_item!
+	@cart_item=CartItem.find_by(item_id: params[:item_id])
+end
 
 def cart_item_params
 	params.require(:cart_item).permit(:quantity)
