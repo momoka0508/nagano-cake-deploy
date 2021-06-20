65b1728583569217a17421065f50cb9417ee9bd9
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-06-20 18:17:52 +0000

    Add 会員側の注文履歴一覧・詳細controller

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,7 @@
 class Public::OrdersController < ApplicationController
 
+  before_action :authenticate_customer!
+
 	def new
 		@order=Order.new
 	end
@@ -17,16 +19,20 @@ class Public::OrdersController < ApplicationController
 	def complete
 	end
 
+#会員の注文履歴一覧表示(うえ)
 	def index
+		@orders = current_customer.orders
 	end
 
 	def show
+		@order = Order.find(params[:id])
+    @item_orders = @order.item_orders
 	end
 
 private
 
-def order_params
-	params.require(:order).permit(:pay_method, :zip_code, :address, :name)
-end
+	def order_params
+		params.require(:order).permit(:pay_method, :zip_code, :address, :name)
+	end
 
 end
