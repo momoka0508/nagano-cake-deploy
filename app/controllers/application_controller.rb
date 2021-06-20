27d49f651d24539e655d085c442f6ec5e0a74857
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-06-20 01:55:00 +0000

    Add ログイン後の遷移先指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
   helper_method :current_cart
-  
+
+  def after_sign_in_path_for(resource)
+    case resource
+    when Customer
+      items_path
+    when Admin
+      admin_item_path
+    end
+  end
+
   def current_cart
   	if session[:cart_id]
   		@cart = Cart.find(session[:cart_id])
