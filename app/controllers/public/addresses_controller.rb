2a36fa2db6c09c195375cd66ac511298d61bd84c
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-21 07:50:18 +0000

    [Add] before_action :authenticate_customers! addressesコントローラー

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,14 +1,15 @@
 class Public::AddressesController < ApplicationController
-  
+  before_action :authenticate_customer!
+
   def index
     @address = Address.new
     @addresses = Address.where(customer: current_customer)
   end
-  
+
   def edit
     @address = Address.find(params[:id])
   end
-  
+
   def create
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
@@ -19,11 +20,11 @@ class Public::AddressesController < ApplicationController
       render :index
     end
   end
-  
+
   private
-  
+
   def address_params
     params.require(:address).permit(:zip_code, :address, :name)
   end
-  
+
 end
