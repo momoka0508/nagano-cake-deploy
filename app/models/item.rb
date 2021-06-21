00386af0a08a4ff818680253a8eff865e0f43cbf
M-Abe47 <abuzhengze5@gmail.com>
2021-06-21 01:34:17 +0000

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,9 @@ class Item < ApplicationRecord
   belongs_to :type
 	has_many :cart_item, dependent: :destroy
   attachment :image
+  
+  validates :name, presence: true
+  validates :body, presence: true
+  validates :price, presence: true
+  validates :is_active, presence: true
 end
