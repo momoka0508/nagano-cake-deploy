4b3da8fcdba2ddd8068c9a2417023ef3dd6faef2
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-06-22 16:07:08 +0000

    Add 注文ステータス

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,12 +1,22 @@
 class Admin::OrdersController < ApplicationController
-  
+
   def index
     @orders = Order.all
   end
-  
+
   def show
     @order = Order.find(params[:id])
     @items = @order.order_items
   end
-  
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admin_order_path(@order)
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:order_status)
+  end
 end
