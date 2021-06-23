df59b70d47e38f3e402053e9e6fa372b3f914465
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-23 07:59:16 +0000

    [Fix]

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -26,7 +26,7 @@ class Admin::CustomersController < ApplicationController
   private
 
   def customer_params
-    params.rquire(:customer).permit(
+    params.require(:customer).permit(
       :first_name, :last_name, :first_name_kana, :last_name_kana, :zip_code, :address, :phone_number, :email, :is_deleted)
   end
 
