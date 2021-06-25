de08dcb9c2f5e433bde7950a6f4e89aebde0baac
M-Abe47 <abuzhengze5@gmail.com>
2021-06-25 02:37:44 +0000

    基本検索

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,7 @@ class SearchController < ApplicationController
 		@model = params[:model]
 		@content = params[:content]
 		if @model == 'customer'
-			@records = Customer.where(['first_name LIKE ? OR last_name LIKE ?', '%'+@content+'%', '%'+@content+'%'])
+			@records = Customer.where(['(last_name + first_name) LIKE ? OR first_name LIKE ? OR last_name LIKE ?', '%'+@content+'%', '%'+@content+'%', '%'+@content+'%'])
 		else
 			@records = Item.where('name LIKE ?', '%'+@content+'%')
 		end
